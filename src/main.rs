cf9ac79937f52bf773037efc4d6eb1701e787b9c
creativcoder <creativcoders@gmail.com>
2021-08-16 16:32:45 +0530

    Finish clinews

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,40 @@
 use std::error::Error;
 
+use colour::{dark_green, yellow};
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
 struct Articles {
-    articles: Vec<Article>
+    articles: Vec<Article>,
 }
 
+#[derive(Deserialize, Debug)]
 struct Article {
     title: String,
-    url: String
+    url: String,
 }
 
 fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
     let response = ureq::get(url).call()?.into_string()?;
 
-    dbg!(response);
+    let articles: Articles = serde_json::from_str(&response)?;
 
-    todo!()
+    Ok(articles)
 }
 
-fn main() {
-    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=d529f926002e4ed19607bc0e04202c21";
-    let articles = get_articles(url);
+fn render_articles(articles: &Articles) {
+    for i in &articles.articles {
+        dark_green!("> {}\n", i.title);
+        yellow!("- {}\n\n", i.url);
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let url =
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=d529f926002e4ed19607bc0e04202c21";
+    let articles = get_articles(url)?;
+
+    render_articles(&articles);
+
+    Ok(())
 }
