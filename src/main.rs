ee1d8e8bd2749c2e28f8b247e84f52016e277057
creativcoder <creativcoders@gmail.com>
2021-08-29 00:40:15 +0530

    r3-4 load api key and termimad integration

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
-use std::error::Error;
+mod theme;
 
-use colour::{dark_green, yellow};
+use std::error::Error;
+use dotenv::dotenv;
 
 use newsapi::{get_articles, Articles};
 
 fn render_articles(articles: &Articles) {
+    let theme = theme::default();
+    theme.print_text("# Top headlines\n\n");
     for i in &articles.articles {
-        dark_green!("> {}\n", i.title);
-        yellow!("- {}\n\n", i.url);
+        theme.print_text(&format!("`{}`", i.title));
+        theme.print_text(&format!("> *{}*", i.url));
+        theme.print_text("---");
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    dotenv()?;
+
+    let api_key = std::env::var("API_KEY")?;
+    
     let url =
-        "https://newsapi.org/v2/top-headlines?country=us&apiKey=d529f926002e4ed19607bc0e04202c21";
-    let articles = get_articles(url)?;
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=";
+
+    let url = format!("{}{}", url, api_key);
+
+    let articles = get_articles(&url)?;
 
     render_articles(&articles);
 
---
+++ src/theme.rs
@@ -0,0 +1,32 @@
+
+use crossterm::style::Color::{AnsiValue, Green, Rgb, Yellow};
+use termimad::{rgb, MadSkin, StyledChar};
+
+pub fn default() -> MadSkin {
+    let mut skin = MadSkin::default();
+    skin.bold.set_fg(Yellow);
+    skin.italic.set_bg(Rgb {
+        r: 28,
+        g: 28,
+        b: 28,
+    });
+    skin.bullet = StyledChar::from_fg_char(Yellow, '⟡');
+    skin.set_headers_fg(Yellow);
+    skin.quote_mark = StyledChar::from_fg_char(Yellow, '▐');
+    skin.quote_mark.set_fg(Rgb {
+        r: 215,
+        g: 255,
+        b: 135,
+    });
+    skin.inline_code.set_fg(Rgb {
+        r: 255,
+        g: 0,
+        b: 200,
+    });
+    skin.italic.set_fg(Rgb {
+        r: 215,
+        g: 255,
+        b: 0,
+    });
+    skin
+}
